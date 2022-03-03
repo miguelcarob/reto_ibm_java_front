fd4fdc169d1f1dfb0b959ba27aef511f86b03a38
miguelcarob <miguelcaro@ibm.com>
2022-03-03 09:15:39 -0500

    Update app.component.html

--- cinemaAppv1/src/app/shared/models/Category.model.ts
+++ cinemaAppv1/src/app/shared/models/Category.model.ts
@@ -1,5 +1,11 @@
-export interface Category{
-    id: number;
-    nameCategory:string;
+export class Category {
 
+    id?: number;
+    nameCategory: string;
+
+    constructor(id: number = null,
+                nameCategory: string = '') {
+        this.id = id;
+        this.nameCategory = nameCategory;
+    }
 }
